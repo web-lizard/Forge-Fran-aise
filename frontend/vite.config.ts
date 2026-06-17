f6fe06d9a3b1287a848ef86bbf2c753fc65ff52b
web-lizard <web-lizard@users.noreply.github.com>
2026-06-17 09:15:59 +0300

    hotfix: restore vue plugin config and not found page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue],
+  plugins: [vue()],
+  server: {
+    host: '127.0.0.1',
+    port: 5197,
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 4197,
+  },
 })
